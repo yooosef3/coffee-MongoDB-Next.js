4b2ec2e2d2ed9aaf64e624edeb2ed13ad726b05d
Yusef Khedri <yooosef.khedri@gmail.com>
2023-08-02 00:18:52 +0330

    some files changed

--- pages/success.tsx
+++ pages/success.tsx
@@ -12,9 +12,9 @@ const Success = () => {
   };
   return (
     <div className="h-screen flex gap-10 flex-col justify-center items-center">
-      <div className="flex gap-3 justify-center items-center bg-slate-600 rounded-xl p-10">
+      <div className="flex gap-3 justify-center items-center bg-green-200 rounded-xl p-10 animate-bounce">
         <AiFillCheckCircle className="text-4xl text-green-600" />
-        <h1 className="font-bold text-lg lg:text-3xl">پرداخت شما با موفقیت انجام شد!</h1>
+        <h1 className="font-bold text-lg lg:text-3xl text-green-700">پرداخت شما با موفقیت انجام شد!</h1>
       </div>
 
       <p
